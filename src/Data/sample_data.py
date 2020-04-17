e39560891518794204e5900b1cbbbf8fc4913c7f
math <mateusz.kaczorek.94@gmail.com>
2020-04-17 18:55:37 +0200

    Fixed data generation for binary calculator.

--- src/Data/sample_data.py
+++ src/Data/sample_data.py
@@ -4,14 +4,27 @@
 #   user should prepare give data file
 # ---------------------------------------- #
 
-
-# TODO: Generate binary data
-
 from random import randint
+import numpy as np
 
 input_size = 5  # number of input data in one set
 number_of_inputs = 50  # number of data sets to supply the net
 
-data_in = [[randint(0, 100) for x in range(input_size)] for y in range(number_of_inputs)]  # randomly generated inputs
+data_out = []
 
-data_out = [sum(data_in[i]) for i in range(len(data_in))]  # output data - in this case we're using sum and we expect the Net to "learn" how to add input values. The function can be changed of course
+for line in range(number_of_inputs):
+
+    dataset = [randint(0, 1) for x in range(input_size)]
+    gate = dataset[0]
+
+    for i in range(1, input_size):
+        if dataset[i] == 1 and gate == 1:
+            gate = 1
+        else:
+            gate = 0
+    dataset.append(gate)
+    data_out.append(dataset)
+
+data_out = np.array(data_out)
+
+np.savetxt('input_data.txt', data_out, fmt='%d', delimiter=',')

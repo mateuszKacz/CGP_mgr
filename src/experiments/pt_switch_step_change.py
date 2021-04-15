af1719a07f755fd3d473591227fe6d8b1488a962
math <mateusz.kaczorek.94@gmail.com>
2021-04-15 22:20:39 +0200

    Experiments - parallel tempering

--- src/experiments/pt_switch_step_change.py
+++ src/experiments/pt_switch_step_change.py
@@ -8,6 +8,7 @@ from ..user_inputs import objective_function as obj_func
 from ..user_inputs import gate_functions as gate_func
 import pathlib
 from ..data_gather.data_gathering_automat import gen_cgp_data, save_to_csv
+from tqdm import tqdm
 
 
 def main():
@@ -17,19 +18,23 @@ def main():
     gate_fun = [gate_func.bin_and, gate_func.bin_nand, gate_func.bin_or, gate_func.bin_xor]
     data_even_check_gate = genfromtxt(MAIN_USER_INPUT_PATH / 'input_even_check_gate.txt', delimiter=',')
 
-    temperatures = [10 * x for x in range(1, 8)]
-    num_sim = 300
-    switch_step = 10
+    temperatures = [x * 0.1 for x in range(1, 70, 10)]
+    num_sim = 30
+    switch_steps = [x for x in range(1, 50, 5)]
     steps = 5000
     annealing_scheme = ['const']
     pt_scheme = ['gaussian', 1]
 
-    data = gen_cgp_data(_gate_func=gate_fun, _obj_func=obj_func.obj_func, _data=data_even_check_gate,
-                        _input_data_size=5, _steps=steps, _annealing_scheme=annealing_scheme, _pt_temps=temperatures,
-                        _algorithm='PT', _pt_switch_step=switch_step, _pt_scheme=pt_scheme, _show_progress=False,
-                        _num_of_sim=num_sim)
+    all_data = []
+    for switch_step in tqdm(switch_steps, desc='Switch step: '):
+        data = gen_cgp_data(_gate_func=gate_fun, _obj_func=obj_func.obj_func, _data=data_even_check_gate,
+                            _input_data_size=5, _steps=steps, _annealing_scheme=annealing_scheme, _pt_temps=temperatures,
+                            _algorithm='PT', _pt_switch_step=switch_step, _pt_scheme=pt_scheme, _show_progress=False,
+                            _num_of_sim=num_sim)
 
-    save_to_csv(data, MAIN_DATA_GATHER_PATH / f'pt_example_numsim{num_sim}_switch_step{switch_step}_steps{steps}_ptscheme{pt_scheme[0]}{pt_scheme[1]}_sa{annealing_scheme[0]}.csv')
+        all_data += data
+
+    save_to_csv(all_data, MAIN_DATA_GATHER_PATH / f'pt_example_numsim{num_sim}_switch_steps1_50_steps{steps}_ptscheme{pt_scheme[0]}{pt_scheme[1]}_sa{annealing_scheme[0]}.csv')
 
 
 if __name__ == "__main__":
--- src/experiments/sa_example_experiment.py
+++ src/experiments/sa_example_experiment.py
@@ -13,14 +13,15 @@ from ..data_gather import data_gathering_automat as data_gather
 
 def main():
     MAIN_USER_INPUT_PATH = pathlib.Path().absolute() / 'src/user_inputs'
+    OUTPUT_PATH = pathlib.Path().absolute() / 'src/data_gather/cgp/EVENCHECKgate'
 
     gate_fun = [gate_func.bin_and, gate_func.bin_nand, gate_func.bin_or, gate_func.bin_xor]
     data_even_check_gate = genfromtxt(MAIN_USER_INPUT_PATH / 'input_even_check_gate.txt', delimiter=',')
 
     cgpsa = CGPSA(_gate_func=gate_fun, _obj_func=obj_func.obj_func, _data=data_even_check_gate, _input_data_size=5,
-                  _steps=5000, _data_gather_interval=1, _annealing_scheme=['linear'])
-    cgpsa.run()
-    data_gather.save_to_csv(cgpsa.simulation.get_params_history(), "every_step_momentum_data_evencheck_gate_linear_sa.txt")
+                  _steps=5000, _data_gather_interval=1, _annealing_scheme=None)
+    cgpsa.run(show_progress=True)
+    data_gather.save_to_csv(cgpsa.simulation.get_params_history(), OUTPUT_PATH / "every_step_momentum_data_evencheck_gate_cgp_alone_sa.txt")
 
 
 if __name__ == "__main__":
